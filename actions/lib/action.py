ed353064454403583943a9d6db52a8fc0ec38c48
Nick Maludy <nmaludy@gmail.com>
2017-06-30 00:05:59 -0400

    Convert results from digitalocean objects to dicts and lists

--- actions/lib/action.py
+++ actions/lib/action.py
@@ -1,6 +1,14 @@
 import digitalocean
+import json
 from st2actions.runners.pythonrunner import Action
 
+
+def digitalocean_obj_to_dict(obj):
+    obj_dict = obj.__dict__
+    # digitalocean.baseapi.BaseAPI has a field named _log that doesn't like to be serialized
+    if "_log" in obj_dict:
+        del obj_dict['_log']
+    return obj_dict
 
 class BaseAction(Action):
 
@@ -10,4 +18,10 @@ class BaseAction(Action):
 
     def do_action(self, cls, action, **kwargs):
         obj = getattr(digitalocean, cls)(token=self._token)
-        return getattr(obj, action)(**kwargs)
+        digitalocean_results = getattr(obj, action)(**kwargs)
+        # The results received from the digitalocean module are
+        # custom python objects that StackStorm doesn't recognize.
+        # The following code converts them into dicts & lists
+        json_str = json.dumps(digitalocean_results, default=digitalocean_obj_to_dict)
+        results = json.loads(json_str)
+        return (True, results)
